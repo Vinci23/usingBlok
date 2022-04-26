cc3d7cd9b49ce5d8636d2c66a6204db17f861073
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-04-26 13:09:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usingBlok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Unesite ime: ");
+            string ime= Console.ReadLine();
+
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+
+            Console.WriteLine("\n-----------------Upisivanje podataka u datoteku...");
+
+            using (StreamWriter sw = new StreamWriter(@"c:/prviDir/mojaDatoteka.txt"))
+            {
+                sw.WriteLine("Ime: {0}", ime);
+                sw.WriteLine("Prezime: {0}",prezime);
+
+            }
+
+            Console.WriteLine("\n-------Upisano!");
+
+            //čitanje podataka iz datoteke
+
+            Console.WriteLine("\n-----------------Čitanje podataka iz datoteke...");
+
+            using(StreamReader sr = new StreamReader(@"c:/prviDir/mojaDatoteka.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+
+
+
+
+            Console.ReadKey();
+                
+
+        }
+    }
+}
